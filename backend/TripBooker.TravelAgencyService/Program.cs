a56486563bb575ff66e38cf36c95732c367eb152
TPiechocki <t.piechocki@yahoo.com>
2022-04-07 15:37:42 +0200

    Read transport view update events and persist them in local cache db

---
+++ backend/TripBooker.TravelAgencyService/Program.cs
@@ -0,0 +1,52 @@
+using TripBooker.TravelAgencyService.Infrastructure;
+using TripBooker.TravelAgencyService.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services
+    .AddInfrastructure(builder.Configuration)
+    .AddRepositories();
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+CreateDbIfNotExists(app);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapControllers();
+
+app.Run();
+
+static void CreateDbIfNotExists(IHost host)
+{
+    using var scope = host.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    try
+    {
+        var dbContext = services.GetRequiredService<TravelAgencyDbContext>();
+
+        SqlDbInitializer.Initialize(dbContext);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
