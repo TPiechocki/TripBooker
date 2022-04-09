f453ab4f358fd79872ce5a3d6d58f383e0b324ff
TPiechocki <t.piechocki@yahoo.com>
2022-04-09 16:49:12 +0200

    Generator for all saturdays in June-August period

--- backend/TripBooker.TransportService/Infrastructure/TransportsGenerator.cs
+++ backend/TripBooker.TransportService/Infrastructure/TransportsGenerator.cs
@@ -1,4 +1,5 @@
-﻿using TripBooker.TransportService.Contract;
+﻿using TripBooker.Common.Helpers;
+using TripBooker.TransportService.Contract;
 using TripBooker.TransportService.Model;
 
 namespace TripBooker.TransportService.Infrastructure
@@ -8,15 +9,26 @@ namespace TripBooker.TransportService.Infrastructure
         internal static IEnumerable<NewTransportContract> GenerateTransports(IEnumerable<TransportOption> options)
         {
             var random = new Random();
+            var saturdays = DateTimeHelpers.GetDaysBetween(
+                    DateTime.SpecifyKind(new DateTime(DateTime.UtcNow.Year, 06, 01), DateTimeKind.Utc),
+                    DateTime.SpecifyKind(new DateTime(DateTime.UtcNow.Year, 08, 31), DateTimeKind.Utc))
+                .Where(x => x.DayOfWeek == DayOfWeek.Saturday)
+                .ToList();
+
+            var transports = new List<NewTransportContract>();
 
             foreach (var option in options)
             {
-                yield return new NewTransportContract(
-                    option.Id,
-                    DateTime.SpecifyKind(new DateTime(2022, 06, 01), DateTimeKind.Utc),
-                    random.Next(50, 150)
-                );
+                var newTransports = saturdays.Select(x =>
+                    new NewTransportContract(
+                        option.Id,
+                        x,
+                        random.Next(50, 150)));
+
+                transports.AddRange(newTransports);
             }
+
+            return transports;
         }
     }
 }
