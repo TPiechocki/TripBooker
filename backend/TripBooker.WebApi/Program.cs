1b628e8fdf977dfad4ab200782f88381013ae78b
TPiechocki <t.piechocki@yahoo.com>
2022-05-08 14:22:13 +0200

    Fix CORS

--- backend/TripBooker.WebApi/Program.cs
+++ backend/TripBooker.WebApi/Program.cs
@@ -5,8 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using WebApi.Repositories;
 using WebApi.Services;
 
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<IUserRepository, InMemoryUserRepository>();
@@ -15,19 +13,18 @@ builder.Services.AddAuthentication(BasicDefaults.AuthenticationScheme)
     .AddBasic<BasicUserValidationService>(options =>
     {
         options.Realm = "TripBooker";
-        options.Events = new BasicEvents
-        {
-
-        };
+        options.IgnoreAuthenticationIfAllowAnonymous = true;
     });
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy  =>
-                      {
-                          policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
-                      });
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.SetIsOriginAllowed(x => true)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
 });
 
 // Add services to the container.
@@ -61,11 +58,12 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthentication();
 
 app.UseAuthorization();
 
-app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
