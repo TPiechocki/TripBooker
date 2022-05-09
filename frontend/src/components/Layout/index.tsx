cee5754f920968d1b4d5a01debcf36e18d85c99a
grzesiek <grzegorz.chamera@gmail.com>
2022-05-09 21:15:58 +0200

    deleted unused button

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -17,15 +17,6 @@ const Layout = ({children}: LayoutProps) => {
     <>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{mr: 2}}
-          >
-            <MenuIcon/>
-          </IconButton>
           <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
             Trip Booker
           </Typography>
