f568d7952407c5934cc0467748b68b2c0dbcc340
TPiechocki <t.piechocki@yahoo.com>
2022-05-30 14:29:13 +0200

    Empty statistics service

---
+++ backend/TripBooker.StatisticsService/Infrastructure/InfrastructureRegistration.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+
+namespace TripBooker.StatisticsService.Infrastructure;
+
+internal static class InfrastructureRegistration
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services
+            .AddBus(configuration);
+    }
+
+    private static IServiceCollection AddBus(this IServiceCollection services, IConfiguration configuration)
+    {
+        var host = configuration.GetSection("RabbitMq")["Host"];
+
+        return services.AddMassTransit(x =>
+            {
+                // PUBLIC
+
+                x.UsingRabbitMq((context, cfg) =>
+                    {
+                        cfg.Host(host);
+                        cfg.ConfigureEndpoints(context);
+                    }
+                );
+            })
+            .Configure<MassTransitHostOptions>(x => { x.WaitUntilStarted = true; });
+        ;
+    }
+}
