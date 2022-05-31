3b443d50fd0ddde990949bc96134389b5ff1b6f8
Maciej <51022993+MrCorncrake@users.noreply.github.com>
2022-05-31 11:56:36 +0200

    Fixed AutoMapper

---
+++ backend/TripBooker.TourOperator/Model/Mappings/HotelMappings.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TripBooker.Common.Hotel.Contract;
+
+namespace TripBooker.TourOperator.Model.Mappings;
+
+internal class HotelMappings : Profile
+{
+    public HotelMappings()
+    {
+        CreateMap<HotelOccupationViewContract, HotelOccupationModel>();
+        CreateMap<HotelOccupationModel, HotelOccupationViewContract>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+    }
+}
