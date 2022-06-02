3f1d50636b530dc4467cd9af32521cd29360647c
Maciej <51022993+MrCorncrake@users.noreply.github.com>
2022-06-02 15:13:12 +0200

    Added UpdateController with endpoints for user-defined updates

---
+++ backend/TripBooker.WebApi/Controllers/UpdateController.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TripBooker.Common.TourOperator.Contract.Query;
+
+namespace TripBooker.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[AllowAnonymous]
+public class UpdateController : ControllerBase
+{
+
+    private readonly IBus _bus;
+
+    public UpdateController(IBus bus)
+    {
+        _bus = bus;
+    }
+
+    [HttpPost("Hotel")]
+    public async Task<IActionResult> Hotel(HotelUpdateQuery query, CancellationToken cancellationToken)
+    {
+        await _bus.Publish(query, cancellationToken);
+
+        return Ok();
+    }
+
+    [HttpPost("Transport")]
+    public async Task<IActionResult> Transport(TransportUpdateQuery query, CancellationToken cancellationToken)
+    {
+        await _bus.Publish(query, cancellationToken);
+
+        return Ok();
+    }
+}
