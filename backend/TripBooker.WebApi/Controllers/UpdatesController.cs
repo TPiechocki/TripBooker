1a1fa042d66d7621a20b19dc5f4fe64e98b14ed6
Maciej <51022993+MrCorncrake@users.noreply.github.com>
2022-06-03 14:07:40 +0200

    Added edpoint to get last 10 updates from TO

---
+++ backend/TripBooker.WebApi/Controllers/UpdatesController.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TripBooker.Common.TourOperator.Contract.Query;
+
+namespace TripBooker.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[AllowAnonymous]
+public class UpdatesController : Controller
+{
+    private readonly IRequestClient<UpdatesQueryContract> _client;
+
+    public UpdatesController(
+        IRequestClient<UpdatesQueryContract> client)
+    {
+        _client = client;
+    }
+
+    [HttpGet]
+    public async Task<UpdatesQueryResultContract> Get(CancellationToken cancellationToken)
+    {
+        var response = await _client.GetResponse<UpdatesQueryResultContract>(
+            new UpdatesQueryContract(), cancellationToken);
+        return response.Message;
+    }
+}
